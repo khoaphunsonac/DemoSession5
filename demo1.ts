6653eda5a15a90145bd2494bd46912be72d30398
khoaphunsonac <khoa.trandang020704@hcmut.edu.vn>
2024-09-30 10:06:49 +0700

    Add DemoSession5 folder

---
+++ demo1.ts
@@ -0,0 +1,12 @@
+function Hello(){
+    console.log("Hello World");
+}
+// Gọi hàm để thực hiện 
+Hello();
+
+function Hi(){
+    console.log("Hi");
+}
+Hi();
+
+
---
+++ demo2.ts
@@ -0,0 +1,12 @@
+// Truyền tham số vào hàm 
+function Hello(name) {
+    console.log("Hello " + name);
+}
+Hello('Peter');
+// Hàm tính hai số 
+function Sum(a, b) {
+    return a + b;
+}
+let s = Sum(5, 10);
+console.log(s);
+
