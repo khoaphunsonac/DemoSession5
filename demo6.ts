d9c6fceeb38a79e2d08fe164ea2376b0460de79c
khoaphunsonac <khoa.trandang020704@hcmut.edu.vn>
2024-10-02 10:10:46 +0700

    Update demo6.ts file

--- demo6.ts
+++ demo6.ts
@@ -4,8 +4,11 @@ function add(a, b) {
 }
 
 function isEven(number) {
-    if (number % 2 == 0) return true;
-    return false;
+    return number % 2 == 0;
+}
+
+function login(username,password){ 
+    return username == "khoaphunsonac" && password == "blabla";
 }
 
 let sum = add(5, 10);
@@ -13,3 +16,13 @@ console.log(sum);
 
 if (add(10, -5) > 0) console.log("Kết quả là số dương!");
 else console.log("Kết quả là số âm!");
+
+let num = 11;
+if (isEven(num)) console.log(`${num} là số chẵn`);
+else console.log(`${num} là số lẻ`);
+
+let username = 'khoaphunsonac';
+let password = 'blabla';
+
+if(login(username,password)) console.log ("Tài khoản hợp lệ!");
+else console.log("Tài khoản không hợp lệ!");
